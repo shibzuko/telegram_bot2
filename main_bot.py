81504fe6fa980be6ca2b1b2029eb627e62f2b3b6
Azamat Shibzuko <shibzuko@gmail.com>
2023-05-20 03:15:38 +0300

    Add new DB

--- main_bot.py
+++ main_bot.py
@@ -1,7 +1,9 @@
 # Import the necessary modules
 import logging
+import psycopg2
+import os
+from xmlrpc.client import Boolean
 import pytz                            # Библиотека для работы с часовым поясом
-from datetime import datetime
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from os import getenv                  #Позволяет получать значения переменных окружения
@@ -20,11 +22,17 @@ from sqlalchemy.ext.declarative import declarative_base        # Импорт м
 from sqlalchemy.orm import sessionmaker                        # Импорт модуля для работы с сессиями базы данных
 from datetime import datetime                                  # Импорт модуля для работы с датой и временем
 
+load_dotenv('.env')                    #Эта функция загружает значения переменных окружения фвйла .env в текущую среду
 
 # Добавление объявления базы данных и создание таблицы сообщений
 
+database_url = os.environ['DATABASE_URL']  # Получите URL базы данных из переменной окружения
+engine = create_engine(database_url, sslmode='require')  # Создайте соединение с базой данных
 Base = declarative_base()
-engine = create_engine('sqlite:///DataBase.db')  # Создание базы данных SQLite
+
+
+# Base = declarative_base()
+# engine = create_engine(conn)  # Создание базы данных SQLite
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -32,8 +40,14 @@ class Message(Base):
     __tablename__ = 'messages'  # Имя таблицы в базе данных
 
     id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer)
+    message_id = Column(Integer)
     user_id = Column(Integer)
+    is_bot = Column(Integer)
+    first_name = Column(String)
+    last_name = Column(String)
+    username = Column(String)
+    language_code = Column(String)
+    is_premium = Column(Integer)
     text = Column(String)
     date = Column(DateTime, default=datetime.utcnow)
 
@@ -44,10 +58,12 @@ Base.metadata.create_all(engine)
 
 # messages = session.query(Message).all()
 # for message in messages:
-#     print(message.chat_id, message.date, message.user_id, message.id, message.text)
+#     print(f"(message_id: {message.id}, user_id: {message.user_id}, "
+#           f"is_bot: {message.is_bot}, first_name: {message.first_name}, last_name: {message.last_name}, "
+#           f"username: {message.username}, language_code: {message.language_code}, "
+#           f"is_premium: {message.is_premium}, text: {message.text}, date: {message.date})")
 
 
-load_dotenv()                          #Эта функция загружает значения переменных окружения фвйла .env в текущую среду
 BOT_TOKEN = getenv('MY_API_TOKEN_1')   #Получаем значения переменной окружения с именем 'MY_API_TOKEN_1
 
 # Set up logging / Настроить ведение журнала
@@ -66,20 +82,26 @@ formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
 async def echo(message: types.Message):
     # Добавление сохранения сообщений в базу данных
     new_message = Message(
-        chat_id=message.chat.id,
+        message_id=message.chat.id,
         user_id=message.from_user.id,
+        is_bot=message.from_user.is_bot,
+        first_name=message.from_user.first_name,
+        last_name=message.from_user.last_name,
+        username=message.from_user.username,
+        language_code=message.from_user.language_code,
+        is_premium=message.from_user.is_premium,
         text=message.text,
         date=message.date
     )
 
+    # print(f"user_id: {new_message.user_id}, first_name: {new_message.first_name}, "
+    #       f"last_name: {new_message.last_name}, username: {new_message.username}, text: {new_message.text}")
 
-
-    session.add(new_message)
+    session.add(new_message)  #Добавляет новую запись в БД
     session.commit()
     # Вывод сообщения пользователю
     await message.answer(f'Ассаламу алейкум, {message.chat.first_name} {message.chat.last_name} '
                          f'\nСейчас: {formatted_time}')
-
 
 
 # Start the bot using the executor
