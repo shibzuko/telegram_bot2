23b4aeb155cc5a235f330a2f4cddb4f7f9e81ca8
shibzuko <shibzuko@gmail.com>
2023-06-10 01:42:51 +0300

    v-2.2

--- api_vk.py
+++ api_vk.py
@@ -9,7 +9,7 @@ session = vk_api.VkApi(token=ACCESS_KEY)
 # Статус пользователя
 def get_user_status(vk_id = 223134565):
     try:
-        status = session.method('status.get', {'vk_id': vk_id})
+        status = session.method('status.get', {'user_id': vk_id})
         return status
     except ApiError as e:
         if e.code == 200:
@@ -20,7 +20,7 @@ def get_user_status(vk_id = 223134565):
 # Аватарка пользователя
 
 def get_user_avatar(vk_id=223134565):
-    avatar = session.method('photos.get', {'vk_id': vk_id, 'album_id': 'profile'})
+    avatar = session.method('photos.get', {'user_id': vk_id, 'album_id': 'profile'})
     return avatar
 
 # print(get_user_avatar())
@@ -31,7 +31,7 @@ def get_user_avatar(vk_id=223134565):
 # Сохраненки пользователя
 def get_user_photos_saved(vk_id = 223134565):
     try:
-        photos = session.method('photos.get', {'vk_id': vk_id, 'album_id': 'saved', 'count': 10, 'rev': 1})
+        photos = session.method('photos.get', {'owner_id': vk_id, 'album_id': 'saved', 'count': 10, 'rev': 1})
         photo_list = [i['sizes'][-1]['url'] for i in photos['items']]
         return photo_list
     except ApiError as e:
